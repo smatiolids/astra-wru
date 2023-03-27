23515e793607dc782439a14cf40693da2dead243
Samuel Matioli <samuel.matioli@datastax.com>
2023-03-27 09:30:32 -0300

    Env

--- start-cql-proxy.sh
+++ start-cql-proxy.sh
@@ -1,2 +1,2 @@
 source .env
-./cql-proxy/cql-proxy --astra-bundle $ASTRA_BUNDLE --username $ASTRA_CLIENT_ID --password ASTRA_CLIENT_SECRET --track-usage --usage-keyspace $ASTRA_KEYSPACE
+./cql-proxy/cql-proxy --astra-bundle $ASTRA_BUNDLE --username $ASTRA_CLIENT_ID --password $ASTRA_CLIENT_SECRET --track-usage --usage-keyspace $ASTRA_KEYSPACE
